c13582bd96f1ca585d47ca9075ac96d3e421d584
@usman_afridi <usman_afridi@softtns.com>
2024-09-06 11:23:42 +0500

    add

---
+++ pro.js
@@ -0,0 +1,13 @@
+//Task 1 *Basic calculator*
+
+// let a = Number(prompt("enter your first number"));
+// let b = Number(prompt("enter your second number"));
+// alert(a + b);
+
+// Task 2 *personalized Greeting*
+let userName = prompt("Enter your name ")
+let favcoulor = prompt("Enter your favrite clour ")
+let greetingMessege = `hello ${userName} !  ${favcoulor} is your favorite coulor. `;
+alert(greetingMessege);
+
+
